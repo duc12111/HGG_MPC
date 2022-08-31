37d7bd90fed990e3e5c8516960d375b63e27d5b0
hoaquin25 <ge35wir@mytum.de>
2022-08-31 23:51:53 +0200

    Implement no collision enviroment

--- env_ext/fetch/__init__.py
+++ env_ext/fetch/__init__.py
@@ -4,13 +4,16 @@ from .vanilla import VanillaGoalEnv
 from .fixobj import FixedObjectGoalEnv
 from .interval import IntervalGoalEnv
 from .mpc_control import MPCControlGoalEnv
+from .mpc_no_collision import MPCControlGoalNoCollisionEnv
+
 
 def make_env(args):
-	return {
-		'vanilla': VanillaGoalEnv,
-		'fixobj': FixedObjectGoalEnv,
-		'interval': IntervalGoalEnv,
-		'mpc': MPCControlGoalEnv,
-		'col_test': ColTestGoalEnv,
-		'ghgg_custom': GHGGCustomGoalEnv
-	}[args.goal](args)
+    return {
+        'vanilla': VanillaGoalEnv,
+        'fixobj': FixedObjectGoalEnv,
+        'interval': IntervalGoalEnv,
+        'mpc': MPCControlGoalEnv,
+        'col_test': ColTestGoalEnv,
+        'ghgg_custom': GHGGCustomGoalEnv,
+        'mpc_no_collision': MPCControlGoalNoCollisionEnv
+    }[args.goal](args)
